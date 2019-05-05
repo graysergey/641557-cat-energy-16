3d7a2373888ac607116d659f9ce75b8ff416756f
GraySergey <xgortx@gmail.com>
2019-05-05 22:48:15 +0300

    Реализует работу бургер кнопки для меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,18 @@
-let i = 0;
+const toggleButton = document.querySelector('.toggle-menu');
+const menu = document.querySelector('.main-menu');
+
+const toggleMenu = (evt) => {
+  evt.preventDefault();
+  if (evt.keyCode === 13) {
+    menu.classList.toggle('main-menu--show');
+    toggleButton.classList.toggle('toggle-menu--close');
+  }
+  if (evt.type === "click") {
+    menu.classList.toggle('main-menu--show');
+    toggleButton.classList.toggle('toggle-menu--close');
+  }
+}
+
+toggleButton.addEventListener('click', toggleMenu);
+toggleButton.addEventListener('keydown', toggleMenu);
+
